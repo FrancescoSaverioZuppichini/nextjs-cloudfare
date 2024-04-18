713fad1f9742fa1ec62152082d547f9873d7175a
Francesco Zuppichini <francesco.zuppichini@gmail.com>
2024-04-18 21:45:28 +0200

    build pages works locally

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import { SignOut } from "@/components/signout-button";
 import { auth } from "@/lib/auth";
 
+export const runtime = "edge";
+
 export default async function Home() {
   const session = await auth();
   if (!session) return null;
