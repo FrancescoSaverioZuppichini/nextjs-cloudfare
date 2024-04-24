1c2eec6b18e884f69dbef50e1296b1c20f4d39a3
Francesco Zuppichini <francesco.zuppichini@gmail.com>
2024-04-24 21:35:33 +0200

    minor changes

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -14,19 +14,19 @@ export default function RegisterPage() {
   return (
     <div className="container grid h-screen w-screen flex-col items-center justify-center ">
       <Link
-        href="/login"
+        href="/register"
         className={cn(
           buttonVariants({ variant: "ghost" }),
-          "absolute right-4 top-4 md:right-8 md:top-8",
+          "absolute right-4 top-4 md:right-8 md:top-8"
         )}
       >
-        Login
+        Register
       </Link>
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
         <div className="flex flex-col space-y-2 text-center">
           <Home className="mx-auto h-6 w-6" />
           <h1 className="text-2xl font-semibold tracking-tight">
-            Welcome Back!
+            Create a new Account!
           </h1>
           <p className="text-sm text-muted-foreground"></p>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,55 @@
-import { ModeToggle } from "@/providers/mode-toggle";
-import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
+import { MainNav } from "@/components/main-nav";
+
+export default async function IndexPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ModeToggle />
-    </main>
+    <>
+      <header className="sticky top-0 z-40 border-b bg-background">
+        <div className="container flex h-16 items-center justify-between py-4">
+          {/* <MainNav items={landingPageConfig.mainNav} /> */}
+          <Link
+            href="/login"
+            className={cn(buttonVariants({ variant: "link" }))}
+          >
+            Sign In
+          </Link>
+        </div>
+      </header>
+      <section className="grid-background space-y-4 py-6 md:py-16 lg:py-36">
+        <div className="container flex max-w-[64rem] flex-col gap-4">
+          <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
+            Your{" "}
+            <span className="rounded-xl bg-foreground px-2 py-0 text-center text-background">
+              SaSS
+            </span>{" "}
+            template
+          </h1>
+          <p className="leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+            Built with{" "}
+            <a href="" className="font-medium underline underline-offset-4">
+              nextjs
+            </a>{" "}
+            and bla bla bla
+          </p>
+          <div className="space-x-4">
+            <Link
+              href="#pricing"
+              className={cn(buttonVariants({ variant: "ghost", size: "lg" }))}
+            >
+              Pricing
+            </Link>
+            <Link
+              href="/register"
+              className={cn(buttonVariants({ size: "lg" }))}
+            >
+              Sign Up
+            </Link>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
