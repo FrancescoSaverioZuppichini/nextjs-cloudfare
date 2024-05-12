d8b4acf265823bc9aa13fd3eae1bed3dd21ae152
Francesco Zuppichini <francesco.zuppichini@gmail.com>
2024-05-12 22:13:34 +0200

    returned valid json from /me

--- lib/auth.ts
+++ lib/auth.ts
@@ -12,47 +12,47 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     strategy: "jwt",
   },
   callbacks: {
-    //   jwt({ token, user }) {
-    //     if (user) {
-    //       // User is available during sign-in
-    //       token.id = user.id!;
-    //     }
-    //     return token;
-    //   },
-    //   session({ session, token }) {
-    //     session.user.id = token.id;
-    //     return session;
-    //   },
-    // },
-    async jwt({ token, user }) {
-      const dbUser = await db.query.users.findFirst({
-        where: eq(users.email, token.email!),
-      });
-      console.log(dbUser);
-
-      if (!dbUser) {
-        if (user) {
-          // User is available during sign-in
-          token.id = user.id!;
-        }
-        return token;
+    jwt({ token, user }) {
+      if (user) {
+        // User is available during sign-in
+        token.id = user.id!;
       }
-      return {
-        id: dbUser.id,
-        name: dbUser.name,
-        email: dbUser.email,
-        picture: dbUser.image,
-      };
+      return token;
     },
     session({ session, token }) {
-      if (token) {
-        session.user.id = token.id;
-        session.user.name = token.name;
-        session.user.email = token.email!;
-        session.user.image = token.picture;
-      }
-
+      session.user.id = token.id;
       return session;
     },
   },
+  // async jwt({ token, user }) {
+  //   const dbUser = await db.query.users.findFirst({
+  //     where: eq(users.email, token.email!),
+  //   });
+  //   console.log(dbUser);
+
+  //   if (!dbUser) {
+  //     if (user) {
+  //       // User is available during sign-in
+  //       token.id = user.id!;
+  //     }
+  //     return token;
+  //   }
+  //   return {
+  //     id: dbUser.id,
+  //     name: dbUser.name,
+  //     email: dbUser.email,
+  //     picture: dbUser.image,
+  //   };
+  // },
+  // session({ session, token }) {
+  //   if (token) {
+  //     session.user.id = token.id;
+  //     session.user.name = token.name;
+  //     session.user.email = token.email!;
+  //     session.user.image = token.picture;
+  //   }
+
+  //   return session;
+  // },
+  // },
 });
